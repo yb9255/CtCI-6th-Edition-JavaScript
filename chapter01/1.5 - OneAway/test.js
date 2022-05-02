2c454a01298a686a6f6d1f68d762d554681dbec2
임유빈 <im-yoobin@slvitui-MacBook-Air.local>
2022-05-02 10:21:11 +0900

    oneAway

--- chapter01/1.5 - OneAway/test.js
+++ chapter01/1.5 - OneAway/test.js
@@ -34,3 +34,33 @@ console.log(oneAway('pale', 'ple'));
 console.log(oneAway('pales', 'pale'));
 console.log(oneAway('pale', 'bale'));
 console.log(oneAway('pale', 'bake'));
+
+function oneEditAway(str1, str2) {
+  if (Math.abs(str1.length - str2.length) > 1) {
+    return false;
+  }
+
+  const shorter = str1.length > str2.length ? str2 : str1;
+  const longer = str1.length > str2.length ? str1 : str2;
+
+  let index1 = 0;
+  let index2 = 0;
+  let foundDiff = false;
+
+  while (index1 < shorter.length && index2 < longer.length) {
+    if (shorter.charCodeAt(index1) !== longer.charCodeAt(index2)) {
+      if (foundDiff) return false;
+      foundDiff = true;
+
+      if (shorter.length === longer.length) {
+        index1++;
+      }
+    } else {
+      index1++;
+    }
+
+    index2++;
+  }
+
+  return true;
+}
