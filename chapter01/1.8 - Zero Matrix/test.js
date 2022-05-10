d6c726f32b5985feb8091cf9c0935492f2ced25d
임유빈 <im-yoobin@slvitui-MacBook-Air.local>
2022-05-10 10:26:37 +0900

    zero matrix 2

--- chapter01/1.8 - Zero Matrix/test.js
+++ chapter01/1.8 - Zero Matrix/test.js
@@ -28,14 +28,14 @@ function setZeros() {
 //
 
 function nullifyRow(matrix, row) {
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix[0].length; i++) {
     matrix[row][i] = 0;
   }
 }
 
 function nullifyColumn(matrix, col) {
   for (let i = 0; i < matrix.length; i++) {
-    matrix[col][i] = 0;
+    matrix[i][col] = 0;
   }
 }
 
@@ -44,7 +44,7 @@ function setZeros(matrix) {
   let colHasZero = false;
 
   for (let i = 0; i < matrix[0].length; i++) {
-    if (matrix[0][j] === 0) {
+    if (matrix[0][i] === 0) {
       rowHasZero = true;
       break;
     }
@@ -84,5 +84,44 @@ function setZeros(matrix) {
 
   if (colHasZero) {
     nullifyColumn(matrix, 0);
+  }
+}
+
+
+//
+
+
+function setZeros() {
+  let firstRowHasZero = false;
+  let firstColHasZero = false;
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        if (row === 0) firstRowHasZero = true;
+        if (col === 0) firstColHasZero = true;
+
+        matrix[row][0] = 0;
+        matrix[0][col] = 0;
+      }
+    }
+  }
+
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  if (firstRowHasZero) {
+    matrix[0].fill(0);
+  }
+
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
   }
 }
